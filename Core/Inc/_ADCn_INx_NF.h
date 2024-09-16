9063aeaa4309242d84e5ac8a39dea437e20ce3bf
alexmosthe <moshtey@gmail.com>
2024-09-16 15:29:52 +0300

    Sorting and notch filter applications added

---
+++ Core/Inc/_ADCn_INx_NF.h
@@ -0,0 +1,235 @@
+/*
+ * _INx_NF.h
+ *
+ * Created on: Sep 14, 2024
+ * Author: Mwangi Alex. W
+ *
+ * This header file consists of all the variables and pointers that are to be fed to the CMSIS DSP functions for IIR biquad filter implementation-
+ * for a notch filter that attenuates 50hZ powerline interference noise. The variables and pointers are declared in the form of a structure-
+ * upon which every member of the struct is accessed as a pointer. That way, the input arguments of the DSP functions are fed as members of a-
+ * struct.
+ */
+
+#ifndef INC__ADCN_INX_NF_H_
+#define INC__ADCN_INX_NF_H_
+
+// MACROS
+#define NOOFCOEFFICIENTS 5 // The number of coefficients of one biquad filter stage are 5. Total number of coefficients = number of stages X number of coefficients per stage
+#define NOOFS_VARIABLES 2  // The number of variables for each biquad filter are 2. Total number of coefficients = number of stages X number of state variables per stage
+
+
+//INCLUSIONS
+#include "_DMA_Sort.h" // Header file that caters for the sorting of mixed up channel data from the DMA buffer into buffers that contain the data from respective ADC channels
+#include "arm_math.h" // Header file neccessary for running CMSIS DSP functions
+
+//STRUCTS, VARIABLES AND POINTERS
+// The struct that holds all the variables and pointers that are needed to to run the biquad DSP function
+
+typedef struct
+{
+	uint8_t ADC1_IN1_numstages; // The number of biquad stages for the notch filter. One biquad is of the order 2. Using two stages means our filter is of order 4. Declaration with initialization would result in an error
+
+	float32_t  ADC1_IN1_coeffs[2*NOOFCOEFFICIENTS]; // The coefficients for each biquad stage. The coefficients are declared as an array of size-->Total number of coefficients
+
+	const float32_t *ADC1_IN1_pcoeffs; // Pointer to the coefficients array
+
+	float32_t ADC1_IN1_States[2*NOOFS_VARIABLES]; // A buffer of the size-->Total number of states, is declared and all variables initialized to zero
+
+	float32_t *ADC1_IN1_pState; //Pointer to the states array
+
+	float32_t *ADC1_IN1_psrc; // Pointer to the input or source buffer. The source buffer in this case is one that stores sorted data from channel 1 of ADC1
+
+	float32_t _NF_ADC1_IN1_bfr[ADC_DMA_HALFBUFFERSIZE];
+
+	float32_t *ADC1_IN1_pdst; // Pointer to the output buffer
+
+	uint32_t ADC1_IN1_Blocksize; // Number of samples to be processed. Equal to the source buffer size
+
+	const arm_biquad_cascade_df2T_instance_f32 S1; //Instance that is fed to the CMSIS DSP functions
+
+} ADC1_IN1_NF;
+
+extern ADC1_IN1_NF ADC1_IN1_NF_arg;
+
+
+
+typedef struct
+{
+	uint8_t ADC1_IN2_numstages; // The number of biquad stages for the notch filter. One biquad is of the order 2. Using two stages means our filter is of order 4. Declaration with initialization would result in an error
+
+	float32_t  ADC1_IN2_coeffs[2*NOOFCOEFFICIENTS]; // The coefficients for each biquad stage. The coefficients are declared as an array of size-->Total number of coefficients
+
+	const float32_t *ADC1_IN2_pcoeffs; // Pointer to the coefficients array
+
+	float32_t ADC1_IN2_States[2*NOOFS_VARIABLES]; // A buffer of the size-->Total number of states, is declared and all variables initialized to zero
+
+	float32_t *ADC1_IN2_pState; //Pointer to the states array
+
+	float32_t *ADC1_IN2_psrc; // Pointer to the input or source buffer. The source buffer in this case is one that stores sorted data from channel 1 of ADC1
+
+	float32_t _NF_ADC1_IN2_bfr[ADC_DMA_HALFBUFFERSIZE];
+
+	float32_t *ADC1_IN2_pdst; // Pointer to the output buffer
+
+	uint32_t ADC1_IN2_Blocksize; // Number of samples to be processed. Equal to the source buffer size
+
+	const arm_biquad_cascade_df2T_instance_f32 S2; //Instance that is fed to the CMSIS DSP functions
+
+} ADC1_IN2_NF;
+
+extern ADC1_IN2_NF ADC1_IN2_NF_arg;
+
+
+typedef struct
+{
+	uint8_t ADC2_IN3_numstages; // The number of biquad stages for the notch filter. One biquad is of the order 2. Using two stages means our filter is of order 4. Declaration with initialization would result in an error
+
+	float32_t  ADC2_IN3_coeffs[2*NOOFCOEFFICIENTS]; // The coefficients for each biquad stage. The coefficients are declared as an array of size-->Total number of coefficients
+
+	const float32_t *ADC2_IN3_pcoeffs; // Pointer to the coefficients array
+
+	float32_t ADC2_IN3_States[2*NOOFS_VARIABLES]; // A buffer of the size-->Total number of states, is declared and all variables initialized to zero
+
+	float32_t *ADC2_IN3_pState; //Pointer to the states array
+
+	float32_t *ADC2_IN3_psrc; // Pointer to the input or source buffer. The source buffer in this case is one that stores sorted data from channel 1 of ADC1
+
+	float32_t _NF_ADC2_IN3_bfr[ADC_DMA_HALFBUFFERSIZE];
+
+	float32_t *ADC2_IN3_pdst; // Pointer to the output buffer
+
+	uint32_t ADC2_IN3_Blocksize; // Number of samples to be processed. Equal to the source buffer size
+
+	const arm_biquad_cascade_df2T_instance_f32 S3; //Instance that is fed to the CMSIS DSP functions
+
+} ADC2_IN3_NF;
+
+extern ADC2_IN3_NF ADC2_IN3_NF_arg;
+
+
+
+typedef struct
+{
+	uint8_t ADC2_IN4_numstages; // The number of biquad stages for the notch filter. One biquad is of the order 2. Using two stages means our filter is of order 4. Declaration with initialization would result in an error
+
+	float32_t  ADC2_IN4_coeffs[2*NOOFCOEFFICIENTS]; // The coefficients for each biquad stage. The coefficients are declared as an array of size-->Total number of coefficients
+
+	const float32_t *ADC2_IN4_pcoeffs; // Pointer to the coefficients array
+
+	float32_t ADC2_IN4_States[2*NOOFS_VARIABLES]; // A buffer of the size-->Total number of states, is declared and all variables initialized to zero
+
+	float32_t *ADC2_IN4_pState; //Pointer to the states array
+
+	float32_t *ADC2_IN4_psrc; // Pointer to the input or source buffer. The source buffer in this case is one that stores sorted data from channel 1 of ADC1
+
+	float32_t _NF_ADC2_IN4_bfr[ADC_DMA_HALFBUFFERSIZE];
+
+	float32_t *ADC2_IN4_pdst; // Pointer to the output buffer
+
+	uint32_t ADC2_IN4_Blocksize; // Number of samples to be processed. Equal to the source buffer size
+
+	const arm_biquad_cascade_df2T_instance_f32 S4; //Instance that is fed to the CMSIS DSP functions
+
+} ADC2_IN4_NF;
+
+extern ADC2_IN4_NF ADC2_IN4_NF_arg;
+
+
+
+typedef struct
+{
+	uint8_t ADC3_IN1_numstages; // The number of biquad stages for the notch filter. One biquad is of the order 2. Using two stages means our filter is of order 4. Declaration with initialization would result in an error
+
+	float32_t  ADC3_IN1_coeffs[2*NOOFCOEFFICIENTS]; // The coefficients for each biquad stage. The coefficients are declared as an array of size-->Total number of coefficients
+
+	const float32_t *ADC3_IN1_pcoeffs; // Pointer to the coefficients array
+
+	float32_t ADC3_IN1_States[2*NOOFS_VARIABLES]; // A buffer of the size-->Total number of states, is declared and all variables initialized to zero
+
+	float32_t *ADC3_IN1_pState; //Pointer to the states array
+
+	float32_t *ADC3_IN1_psrc; // Pointer to the input or source buffer. The source buffer in this case is one that stores sorted data from channel 1 of ADC1
+
+	float32_t _NF_ADC3_IN1_bfr[ADC_DMA_HALFBUFFERSIZE];
+
+	float32_t *ADC3_IN1_pdst; // Pointer to the output buffer
+
+	uint32_t ADC3_IN1_Blocksize; // Number of samples to be processed. Equal to the source buffer size
+
+	const arm_biquad_cascade_df2T_instance_f32 S5; //Instance that is fed to the CMSIS DSP functions
+
+} ADC3_IN1_NF;
+
+extern ADC3_IN1_NF ADC3_IN1_NF_arg;
+
+
+typedef struct
+{
+	uint8_t ADC3_IN2_numstages; // The number of biquad stages for the notch filter. One biquad is of the order 2. Using two stages means our filter is of order 4. Declaration with initialization would result in an error
+
+	float32_t  ADC3_IN2_coeffs[2*NOOFCOEFFICIENTS]; // The coefficients for each biquad stage. The coefficients are declared as an array of size-->Total number of coefficients
+
+	const float32_t *ADC3_IN2_pcoeffs; // Pointer to the coefficients array
+
+	float32_t ADC3_IN2_States[2*NOOFS_VARIABLES]; // A buffer of the size-->Total number of states, is declared and all variables initialized to zero
+
+	float32_t *ADC3_IN2_pState; //Pointer to the states array
+
+	float32_t *ADC3_IN2_psrc; // Pointer to the input or source buffer. The source buffer in this case is one that stores sorted data from channel 1 of ADC1
+
+	float32_t _NF_ADC3_IN2_bfr[ADC_DMA_HALFBUFFERSIZE];
+
+	float32_t *ADC3_IN2_pdst; // Pointer to the output buffer
+
+	uint32_t ADC3_IN2_Blocksize; // Number of samples to be processed. Equal to the source buffer size
+
+	const arm_biquad_cascade_df2T_instance_f32 S6; //Instance that is fed to the CMSIS DSP functions
+
+} ADC3_IN2_NF;
+
+extern ADC3_IN2_NF ADC3_IN2_NF_arg;
+
+//FUNCTION DECLARATIONS
+void init_ADC1_IN1_struct(void);
+
+void init_ADC1_IN1_FO_biquad_filter(void);
+
+void update_ADC1_IN1_FO_biquad_filter(void);
+
+
+void init_ADC1_IN2_struct(void);
+
+void init_ADC1_IN2_FO_biquad_filter(void);
+
+void update_ADC1_IN2_FO_biquad_filter(void);
+
+
+void init_ADC2_IN3_struct(void);
+
+void init_ADC2_IN3_FO_biquad_filter(void);
+
+void update_ADC2_IN3_FO_biquad_filter(void);
+
+
+void init_ADC2_IN4_struct(void);
+
+void init_ADC2_IN4_FO_biquad_filter(void);
+
+void update_ADC2_IN4_FO_biquad_filter(void);
+
+
+void init_ADC3_IN1_struct(void);
+
+void init_ADC3_IN1_FO_biquad_filter(void);
+
+void update_ADC3_IN1_FO_biquad_filter(void);
+
+
+void init_ADC3_IN2_struct(void);
+
+void init_ADC3_IN2_FO_biquad_filter(void);
+
+void update_ADC3_IN2_FO_biquad_filter(void);
+
+
+#endif /* INC__ADCN_INX_NF_H_ */
---
+++ Core/Src/_DMA_Sort.c
@@ -0,0 +1,144 @@
+/*
+ * _DMA_Sort.c
+ *
+ *  Created on: Sep 14, 2024
+ *  Author: Mwangi Alex. W
+ *
+ *  Defines all the functions that implement the sorting algorithm for all ADCs
+ */
+#include "_DMA_Sort.h"
+
+void ADC1_DMA_sort_uhb (void)
+{
+	for(uint32_t m=0; m<ADC_DMA_HALFBUFFERSIZE-1; m++)
+	{
+		if(m==0 || (m%2==0))
+		{
+			for(uint32_t n=0; n<ADC_DMA_QUATERBUFFERSIZE-1; n++)
+			{
+				ADC1_DMA_sort_ptr->ADC1_IN1_bfr[n]=ADC1_DMA_sort_ptr->ADC1_DMA_bfr[m];
+            }
+        }
+		else
+		{
+			for(uint32_t p=0; p<ADC_DMA_QUATERBUFFERSIZE-1; p++)
+			{
+				ADC1_DMA_sort_ptr->ADC1_IN2_bfr[p]=ADC1_DMA_sort_ptr->ADC1_DMA_bfr[m];
+			}
+		}
+    }
+
+};
+
+void ADC1_DMA_sort_lhb (void)
+{
+	for(uint32_t m=ADC_DMA_HALFBUFFERSIZE; m<ADC_DMA_BUFFERSIZE-1; m++)
+	{
+		if(m==0||(m%2==0))
+		{
+			for(uint32_t n=ADC_DMA_QUATERBUFFERSIZE; n<ADC_DMA_HALFBUFFERSIZE-1; n++)
+			{
+				ADC1_DMA_sort_ptr->ADC1_IN1_bfr[n]=ADC1_DMA_sort_ptr->ADC1_DMA_bfr[m];
+            }
+        }
+		else
+		{
+			for(uint32_t p=ADC_DMA_QUATERBUFFERSIZE; p<ADC_DMA_HALFBUFFERSIZE-1; p++)
+			{
+				ADC1_DMA_sort_ptr->ADC1_IN2_bfr[p]=ADC1_DMA_sort_ptr->ADC1_DMA_bfr[m];
+			}
+		}
+    }
+
+};
+
+
+void ADC2_DMA_sort_uhb (void)
+{
+	for(uint32_t m=0; m<ADC_DMA_HALFBUFFERSIZE-1; m++)
+	{
+		if(m==0 || (m%2==0))
+		{
+			for(uint32_t n=0; n<ADC_DMA_QUATERBUFFERSIZE-1; n++)
+			{
+				ADC2_DMA_sort_ptr->ADC2_IN3_bfr[n]=ADC2_DMA_sort_ptr->ADC2_DMA_bfr[m];
+            }
+        }
+		else
+		{
+			for(uint32_t p=0; p<ADC_DMA_QUATERBUFFERSIZE-1; p++)
+			{
+				ADC2_DMA_sort_ptr->ADC2_IN4_bfr[p]=ADC2_DMA_sort_ptr->ADC2_DMA_bfr[m];
+			}
+		}
+    }
+
+};
+
+
+void ADC2_DMA_sort_lhb (void)
+{
+	for(uint32_t m=ADC_DMA_HALFBUFFERSIZE; m<ADC_DMA_BUFFERSIZE-1; m++)
+	{
+		if(m==0||(m%2==0))
+		{
+			for(uint32_t n=ADC_DMA_QUATERBUFFERSIZE; n<ADC_DMA_HALFBUFFERSIZE-1; n++)
+			{
+				ADC2_DMA_sort_ptr->ADC2_IN3_bfr[n]=ADC2_DMA_sort_ptr->ADC2_DMA_bfr[m];
+            }
+        }
+		else
+		{
+			for(uint32_t p=ADC_DMA_QUATERBUFFERSIZE; p<ADC_DMA_HALFBUFFERSIZE-1; p++)
+			{
+				ADC2_DMA_sort_ptr->ADC2_IN4_bfr[p]=ADC2_DMA_sort_ptr->ADC2_DMA_bfr[m];
+			}
+		}
+    }
+
+};
+
+
+void ADC3_DMA_sort_uhb (void)
+{
+	for(uint32_t m=0; m<ADC_DMA_HALFBUFFERSIZE-1; m++)
+	{
+		if(m==0 || (m%2==0))
+		{
+			for(uint32_t n=0; n<ADC_DMA_QUATERBUFFERSIZE-1; n++)
+			{
+				ADC3_DMA_sort_ptr->ADC3_IN1_bfr[n]=ADC3_DMA_sort_ptr->ADC3_DMA_bfr[m];
+            }
+        }
+		else
+		{
+			for(uint32_t p=0; p<ADC_DMA_QUATERBUFFERSIZE-1; p++)
+			{
+				ADC3_DMA_sort_ptr->ADC3_IN2_bfr[p]=ADC3_DMA_sort_ptr->ADC3_DMA_bfr[m];
+			}
+		}
+    }
+
+};
+
+void ADC3_DMA_sort_lhb (void)
+{
+	for(uint32_t m=ADC_DMA_HALFBUFFERSIZE; m<ADC_DMA_BUFFERSIZE-1; m++)
+	{
+		if(m==0||(m%2==0))
+		{
+			for(uint32_t n=ADC_DMA_QUATERBUFFERSIZE; n<ADC_DMA_HALFBUFFERSIZE-1; n++)
+			{
+				ADC3_DMA_sort_ptr->ADC3_IN1_bfr[n]=ADC3_DMA_sort_ptr->ADC3_DMA_bfr[m];
+            }
+        }
+		else
+		{
+			for(uint32_t p=ADC_DMA_QUATERBUFFERSIZE; p<ADC_DMA_HALFBUFFERSIZE-1; p++)
+			{
+				ADC3_DMA_sort_ptr->ADC3_IN2_bfr[p]=ADC3_DMA_sort_ptr->ADC3_DMA_bfr[m];
+			}
+		}
+    }
+
+};
